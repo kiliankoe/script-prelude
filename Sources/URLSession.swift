cfc454ebc11f60cb36d7fd8a7008e7084843c20a
Kilian Koeltzsch <me@kilian.io>
2016-12-16 18:44:09 +0100

    add synchronousDataTask with request

--- Sources/URLSession.swift
+++ Sources/URLSession.swift
@@ -25,4 +25,30 @@ extension URLSession {
 
         return (data, response, error)
     }
+
+    /// Retrieves the contents of a URL based on the contents of the specificed URLRequest,
+    /// waiting for the request to be completed before returning the results directly.
+    ///
+    /// - Parameter request: Request to be sent
+    /// - Returns: optional raw data, response and error objects
+    public func synchronousDataTask(with request: URLRequest) -> (Data?, URLResponse?, Error?) {
+        let semaphore = DispatchSemaphore(value: 0)
+
+        var data: Data?
+        var response: URLResponse?
+        var error: Error?
+
+        dataTask(with: request) {
+            data = $0
+            response = $1
+            error = $2
+
+            semaphore.signal()
+        }.resume()
+
+        semaphore.wait()
+
+        return (data, response, error)
+    }
+
 }
--- Tests/script-preludeTests/URLSessionTests.swift
+++ Tests/script-preludeTests/URLSessionTests.swift
@@ -2,9 +2,9 @@ import XCTest
 @testable import script_prelude
 
 class URLSessionTests: XCTestCase {
-    func testSynchronousDataTask() {
+    func testSynchronousDataTaskWithURL() {
         guard let url = URL(string: "https://httpbin.org/get") else {
-            XCTFail("misformatted URL")
+            XCTFail("malformed URL")
             return
         }
 
@@ -15,9 +15,24 @@ class URLSessionTests: XCTestCase {
         XCTAssertEqual(response.statusCode, 200)
     }
 
+    func testSynchronousDataTaskWithRequest() {
+        guard let url = URL(string: "https://httpbin.org/get") else {
+            XCTFail("malformed URL")
+            return
+        }
+        let request = URLRequest(url: url)
+
+        guard let response = (URLSession.shared.synchronousDataTask(with: request).1 as? HTTPURLResponse) else {
+            XCTFail("failed on getting response")
+            return
+        }
+        XCTAssertEqual(response.statusCode, 200)
+    }
+
     static var allTests: [(String, (URLSessionTests) -> () throws -> Void)] {
         return [
-            ("testSynchronousDataTask", testSynchronousDataTask),
+            ("testSynchronousDataTaskWithURL", testSynchronousDataTaskWithURL),
+            ("testSynchronousDataTaskWithRequest", testSynchronousDataTaskWithRequest),
         ]
     }
 }
